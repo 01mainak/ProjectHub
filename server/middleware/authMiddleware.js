f42af21c67739370df573b7f8f85c5a6cba3ad8e
Mainak Das <155906354+01mainak@users.noreply.github.com>
2024-08-01 02:22:28 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,15 +1,31 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/user");
 
-const tokenSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "user",
-    unique: true,
-  },
-  token: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 3600 },
-});
+const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY);
+      req.user = await User.findById(decoded._id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
 
-module.exports = mongoose.model("token", tokenSchema);
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(401).json({ message: "Not authorized as an admin" });
+  }
+};
+
+module.exports = { protect, isAdmin };
